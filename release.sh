e19ec2739092251b1a3fc2c5d3bb076bd766f5a6
cjus <carlos.justiniano@gmail.com>
2024-12-13 08:04:19 -0700

    updated release for homebrew

--- release.sh
+++ release.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 VERSION=$(grep '^version = ' Cargo.toml | cut -d '"' -f2)
+echo "Creating tag v$VERSION..."
 git tag "v$VERSION"
 git push origin "v$VERSION"
+
+echo "Calculating SHA256..."
+SHASUM=$(curl -L "https://github.com/cjus/godir/archive/refs/tags/v$VERSION.tar.gz" | shasum -a 256 | cut -d ' ' -f1)
+echo "SHA256: $SHASUM"
+
+echo -n "Press Enter to install via Homebrew..."
+read
+
+brew tap cjus/godir
+brew install godir
 
